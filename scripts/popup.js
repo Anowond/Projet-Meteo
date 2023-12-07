9b63fa39be28ff9b5c3e691705566adf71c2956a
Alain61 <c.alain999@gmail.com>
2023-12-07 15:28:48 +0100

    input principale et input agrandi

---
+++ scripts/popup.js
@@ -0,0 +1,9 @@
+
+    let inputOrigine = document.getElementById("inputOrigine")
+    console.log("ça passe")
+    let popupInput = document.getElementById("popupInput")
+    let inputAgrandi = document.getElementById("inputAgrandi")
+
+    inputOrigine.addEventListener("click", () => {
+        popupInput.style.display = "block";
+    })
