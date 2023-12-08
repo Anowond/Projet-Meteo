93c0cf92d9357912f625921af694d4e8288517c4
Alain61 <c.alain999@gmail.com>
2023-12-08 09:35:16 +0100

    PopUp aggrandir corrigé

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,16 @@
-import { getResponse } from "./api.js"
+import getResponse from "./api.js"
     //recupération des variable par id
     let inputOrigine = document.getElementById("inputOrigine")    
     let popupInput = document.getElementById("popupInput")
     let inputAgrandi = document.getElementById("inputAgrandi")
     let bouton = document.getElementById("boutonGO")
+    popupInput.style.display = "none";
 
     //au click de l'input écran
     inputOrigine.addEventListener("click", () => {
         //apparition du popup input agrandi
         popupInput.style.display = "block";
-    })
+    });
     //récupération des données retourné par l'API
     function searchAPI(){
         //recuperation de la valeur de l'inputagrandi 
