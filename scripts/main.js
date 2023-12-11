5bf3bb87ddbb8119a6d6afe70812635349d470ef
ElementaryLewis <lewis.p.mu@outlook.com>
2023-12-11 11:56:07 +0100

    Ajout des "alt" sur les icons.

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@ let inputAgrandi = document.getElementById("inputAgrandi")
 //let bouton1 = document.getElementById("button")
 let bouton2 = document.getElementById("boutonGO")
 let ville = document.getElementById("ville_localisation")
+let weather_today = document.getElementById("weather_today")
 let temperature = document.getElementById("temperature")
 let wet = document.getElementById("wet_today")
 let wind = document.getElementById("wind_today")
@@ -49,7 +50,9 @@ bouton2.addEventListener("click", async () => {
     let astro = await getAstro(inputAgrandi.value)
     console.log(resultatRetour, astro)
     weatherIconBig.src = resultatRetour.current.condition.icon
+    weatherIconBig.alt = resultatRetour.current.condition.text
     ville.textContent = `${resultatRetour.location.name},${resultatRetour.location.country}`
+    weather_today.textContent = resultatRetour.current.condition.text
     temperature.textContent = `${resultatRetour.current.temp_c}° Celsius`
     wet.textContent = `Humidité: ${resultatRetour.current.humidity}%`
     wind.textContent = `Vent: ${resultatRetour.current.wind_kph}Km/h`
@@ -84,18 +87,22 @@ bouton2.addEventListener("click", async () => {
 
     // Attribution des valeurs tirées de l'API 
     weatherIconLittleMorning.src = resultatRetour.forecast.forecastday[0].hour[6].condition.icon
+    weatherIconLittleMorning.alt = resultatRetour.forecast.forecastday[0].hour[6].condition.text
     weather_morning.textContent = resultatRetour.forecast.forecastday[0].hour[6].condition.text
     temperatureMorning.textContent = resultatRetour.forecast.forecastday[0].hour[6].temp_c
 
     weatherIconLittleAfternoon.src = resultatRetour.forecast.forecastday[0].hour[11].condition.icon
+    weatherIconLittleAfternoon.alt = resultatRetour.forecast.forecastday[0].hour[6].condition.text
     weather_afternoon.textContent = resultatRetour.forecast.forecastday[0].hour[11].condition.text
     temperatureAfternoon.textContent = resultatRetour.forecast.forecastday[0].hour[11].temp_c
 
     weatherIconLittleEvening.src = resultatRetour.forecast.forecastday[0].hour[16].condition.icon
+    weatherIconLittleEvening.alt = resultatRetour.forecast.forecastday[0].hour[6].condition.text
     weather_evening.textContent = resultatRetour.forecast.forecastday[0].hour[16].condition.text
     temperatureEvening.textContent = resultatRetour.forecast.forecastday[0].hour[16].temp_c
 
     weatherIconLittleNight.src = resultatRetour.forecast.forecastday[0].hour[23].condition.icon
+    weatherIconLittleNight.alt = resultatRetour.forecast.forecastday[0].hour[6].condition.text
     weather_night.textContent = resultatRetour.forecast.forecastday[0].hour[23].condition.text
     temperatureNight.textContent = resultatRetour.forecast.forecastday[0].hour[23].temp_c
 
