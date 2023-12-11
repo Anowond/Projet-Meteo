6d8201b266c8ebdfbd09c6ac7b349fe1d69c35ba
Anowond <baroux.maxime2@gmail.com>
2023-12-11 10:26:34 +0100

    tsssss

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
-import {getResponse, getAstro} from "./api.js"
+import { getResponse, getAstro } from "./api.js"
 
-//recupération des variable par id
-//let inputOrigine = document.getElementById("inputOrigine")
+//recupération des éléments html par id pour l'écran principal
+let weatherIconBig = document.getElementById("weatherIconBig")
+let inputOrigine = document.getElementById("inputOrigine")
+let popupInput = document.getElementById("popupInput")
 let inputAgrandi = document.getElementById("inputAgrandi")
 //let bouton1 = document.getElementById("button")
 let bouton2 = document.getElementById("boutonGO")
@@ -13,7 +15,7 @@ let aqi = document.getElementById("aqi_today")
 let uv = document.getElementById("uv_today")
 let sunrise = document.getElementById("soleil_leve")
 let sunset = document.getElementById("soleil_couche")
-/*popupInput.style.display = "none";
+popupInput.style.display = "none";
 
 //au click de l'input écran
 inputOrigine.addEventListener("click", () => {
@@ -24,39 +26,39 @@ inputOrigine.addEventListener("click", () => {
 //récupération des données retourné par l'API
 
 //recuperation de la reponse
-bouton2.addEventListener("click", async () => {
-    let resultatRetour = await getResponse(inputAgrandi.value);
+bouton1.addEventListener("click", async () => {
+    let resultatRetour = await getResponse(inputOrigine.value);
     console.log(resultatRetour)
     ville.textContent = `${resultatRetour.location.name},${resultatRetour.location.country}`
     temperature.textContent = `${resultatRetour.current.temp_c}° Celsius`
-    wet.textContent = `${resultatRetour.current.humidity}%` 
-    wind.textContent = `${resultatRetour.current.wind_kph}Km/h`
-   
+    wet.textContent = `Humidité: ${resultatRetour.current.humidity}%`
+    wind.textContent = `Vent: ${resultatRetour.current.wind_kph}Km/h`
+
     let currentAqi = resultatRetour.current.air_quality["us-epa-index"]
-    console.log(currentAqi)
+    //console.log(currentAqi)
     switch (currentAqi) {
 
-        case 1 :
-            aqi.textContent = "Bonne"
-        break;
-        case 2 :
-            aqi.textContent = "Modérée"
-        break;
-        case 3 :
-            aqi.textContent = "Dégradée"
-        break;
-        case 4 :
-            aqi.textContent = "Nocive"
-        break;
-        case 5 :
-            aqi.textContent = "Trés Nocive"
-        break;
-        case 6 :
-            aqi.textContent = "Dangereuse"
-        break;
+        case 1:
+            aqi.textContent = "AQI: Bonne"
+            break;
+        case 2:
+            aqi.textContent = "AQI: Modérée"
+            break;
+        case 3:
+            aqi.textContent = "AQI: Dégradée"
+            break;
+        case 4:
+            aqi.textContent = "AQI: Nocive"
+            break;
+        case 5:
+            aqi.textContent = "AQI: Trés Nocive"
+            break;
+        case 6:
+            aqi.textContent = "AQI: Dangereuse"
+            break;
     }
     
-    let astro = await getAstro(inputAgrandi.value)
+    let astro = await getAstro(inputOrigine.value)
     console.log(astro)
 })
 
